814797ca652ca29a015d28a1a12adaa897f94d57
Roma Kutischev <rukkola2@gmail.com>
2020-06-18 22:49:21 +0300

    root component with loader and error alerts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { Container, Dimmer, Loader, Message } from "semantic-ui-react";
+import { Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { useFetching } from "./customHooks";
+import { getStaff } from "./redux/actions/staff";
+import { getServices } from "./redux/actions/services";
 import './App.scss';
 
 function App() {
+  const { staff: { staffLoadingState, error: staffError }, services: { servicesLoadingState, error: servicesError } } = useSelector(state => state);
+
+  useFetching(getStaff);
+  useFetching(getServices);
+
   return (
-    <div className="App">
-      
-    </div>
+    <Container className="application">
+      {staffLoadingState === "loading" || servicesLoadingState === "loading" &&
+        <Dimmer active>
+          <Loader>Loading</Loader>
+        </Dimmer>}
+      {staffLoadingState === "failed" || servicesLoadingState === "failed" &&
+        <Message negative>
+          <Message.Header>Looks like, something went wrong</Message.Header>
+          <p>{staffLoadingState === "failed" && staffError}</p>
+          <p>{servicesLoadingState === "failed" && servicesError}</p>
+        </Message>}
+      {staffLoadingState === "succeed" && servicesLoadingState === "succeed" && <>
+        <Route path="/" exact>
+          {/* <HomePage /> */}
+        </Route>
+
+        {/* <Route path="/add">
+          <AddHeroPage />
+        </Route>
+
+        <Route path="/edit/:id">
+          <EditHeroPage />
+        </Route>
+
+        <Route path="/detail/:id">
+          <HeroDetailPage />
+        </Route> */}
+      </>}
+    </Container>
   );
 }
 
